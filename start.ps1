e5ec84d75b8a298c1b7242c20f02719c5c455d96
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-05-12 08:39:50 +0200

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -5,4 +5,3 @@ docker-compose -f docker-compose.test.yml up --build -d
 docker exec p6-bilemo-php-1 php bin/console --env=test doctrine:schema:create
 docker exec p6-bilemo-php-1 php bin/console doctrine:schema:update --force
 docker exec p6-bilemo-php-1 php bin/console doctrine:fixtures:load --no-interaction
-docker-compose -f docker-compose.test.yml exec php ./vendor/bin/phpunit --colors --testdox
