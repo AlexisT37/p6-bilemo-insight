cce2ceb6c3e25b32a76a8bf2d9845ee4cc1a21e4
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-05-12 10:09:35 +0200

    feat: update fixtures to add user
    also remove schema create statements
    add --complete flag as per recommended by cmd

--- start.ps1
+++ start.ps1
@@ -2,6 +2,8 @@
 
 docker-compose -f docker-compose.test.yml up --build -d
 
-docker exec p6-bilemo-php-1 php bin/console --env=test doctrine:schema:create
-docker exec p6-bilemo-php-1 php bin/console doctrine:schema:update --force
+
+docker exec p6-bilemo-php-1 php bin/console doctrine:schema:update --force --complete
+docker exec p6-bilemo-php-1 php bin/console --env=test doctrine:schema:update --force --complete
 docker exec p6-bilemo-php-1 php bin/console doctrine:fixtures:load --no-interaction
+docker exec p6-bilemo-php-1 php bin/console --env=test doctrine:fixtures:load --no-interaction
